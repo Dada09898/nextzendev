545a84dd24362b4e35628b3c6f812733a03260ed
Ankit Kumar <singhkumar50866@gmail.com>
2026-04-30 21:45:08 +0530

    static fix

--- config/settings.py
+++ config/settings.py
@@ -209,6 +209,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -321,3 +322,8 @@ LOGGING = {
         },
     },
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
